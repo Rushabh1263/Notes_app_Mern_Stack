8cd0762ac2f15da4d1ce0bb8b8d9fcd705b6f4bd
Rushabh Shah <srushabh1263@gmail.com>
2024-11-07 17:56:03 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config()
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +14,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
-const dbURI = "mongodb+srv://Rushabh:Rush1234@cluster0.clb1tji.mongodb.net/app";
+const dbURI = process.env.MONGOOSE_CONNECTION ;
 mongoose.connect(dbURI);
 
 // Define Note model
